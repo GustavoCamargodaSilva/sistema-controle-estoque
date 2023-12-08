bffbabcad03a815eb0b6b95af1998e8624473ce7
Gustavo Camargo <104240187+GustavoCamargodaSilva@users.noreply.github.com>
2023-12-08 14:39:13 -0300

    criaçao do projeto

---
+++ controledeestoque/src/test/java/com/domain/controledeestoque/ControledeestoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domain.controledeestoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControledeestoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
